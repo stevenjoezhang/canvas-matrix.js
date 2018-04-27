49976d36e9185ce686460a07d993e808a7a88285
StevenJoeZhang <1119186082@qq.com>
2018-04-28 01:05:10 +0800

    Add files

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Canvas-Matrix
 Provides the background effect like movie "The Matrix" with JavaScript and Canvas.
 
+## Feature
+- Do not depend on jQuery
+- Easy to use
+
 ## How to use
 Download
 ```
@@ -11,3 +15,21 @@ then add this script to your html page:
 ```
 <script src="path/to/canvas-matrix.js"></script>
 ```
+
+## Config
+- `size` font-size px
+- `font` font-name
+For example:
+```
+<script src="path/to/canvas-matrix.js" size="20" font="arial"></script>
+```
+
+## Preview
+![image](sample.png)
+
+## Credits
+* [Mimi](http://zsq.im) Developer of this project.
+
+## License
+Released under the GNU General Public License v3  
+http://www.gnu.org/licenses/gpl-3.0.html
--- canvas-matrix.js
+++ canvas-matrix.js
@@ -1,51 +1,68 @@
 /*
- * This work is under GNU General Public License.
- * Created by Steven Joe 'Mimi' Zhang in 2017.
- * https://galaxymimi.com
+ * canvas-matrix.js
+ * "The Matrix" like effect based on JavaScript and Canvas.
+ * Created by Shuqiao Zhang in 2017.
+ * http://zsq.im
  */
 
-var canvas = document.createElement("canvas");
-var context = canvas.getContext("2d");
-document.getElementsByTagName("body")[0].appendChild(canvas);
-canvas.style.cssText = "position: fixed; top: 0; left: 0; background-color: #111; z-index: -1;";
-var W = window.innerWidth;
-var H = window.innerHeight;
-canvas.width = W;
-canvas.height = H;
-var fontSize = 16;
-var colunms = Math.floor(W / fontSize);
-var drops = [];
-for (var i = 0; i < colunms; i++) {
-  drops.push(Math.floor(canvas.height / fontSize) + 1);
-}
-var str = "QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()1234567890qwertyuiopasdfghjklzxcvbnm";
-function draw() {
-  context.fillStyle = "rgba(0,0,0,0.05)";
-  context.fillRect(0, 0, W, H);
-  context.font = "700 " + fontSize + "px  微软雅黑";
-  context.fillStyle = "#00cc33";
-  for (var i = 0; i < colunms; i++) {
-    var index = Math.floor(Math.random() * str.length);
-    var x = i * fontSize;
-    var y = drops[i] * fontSize;
-    context.fillText(str[index], x, y);
-    if (y >= canvas.height && Math.random() > 0.99) {
-      drops[i] = 0;
-    }
-    drops[i]++;
-  }
-};
+/* 
+ * This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ *	GNU General Public License for more details.
+ */
 
-setInterval(draw, 120);
+(function() {
+	var scripts = document.getElementsByTagName("script"),
+		script = scripts[scripts.length - 1],
+		fontSize = script.getAttribute("size") || 16,
+		fontName = script.getAttribute("font") || "微软雅黑";
 
-window.onresize = function() {
-  W = window.innerWidth;
-  H = window.innerHeight;
-  canvas.width = W;
-  canvas.height = H;
-  colunms = Math.floor(W / fontSize);
-  drops = [];
-  for (var i = 0; i < colunms; i++) {
-    drops.push(Math.floor(canvas.height / fontSize) + 1);
-  }
-}
+	var canvas = document.createElement("canvas"),
+		context = canvas.getContext("2d");
+	document.getElementsByTagName("body")[0].appendChild(canvas);
+	canvas.style.cssText = "position: fixed; top: 0; left: 0; background-color: #111; z-index: -1;";
+	var W = window.innerWidth,
+		H = window.innerHeight;
+	canvas.width = W;
+	canvas.height = H;
+	var colunms = Math.floor(W / fontSize),
+		drops = [],
+		str = "QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()1234567890qwertyuiopasdfghjklzxcvbnm";
+	for (var i = 0; i < colunms; i++) {
+		drops.push(Math.floor(canvas.height / fontSize) + 1);
+	}
+	setInterval(function() {
+		context.fillStyle = "rgba(0,0,0,0.05)";
+		context.fillRect(0, 0, W, H);
+		context.font = "700 " + fontSize + "px " + fontName;
+		context.fillStyle = "#00cc33";
+		for (var i = 0; i < colunms; i++) {
+			var index = Math.floor(Math.random() * str.length);
+			var x = i * fontSize;
+			var y = drops[i] * fontSize;
+			context.fillText(str[index], x, y);
+			if (y >= canvas.height && Math.random() > 0.99) {
+				drops[i] = 0;
+			}
+			drops[i]++;
+		}
+	}, 120);
+
+	window.addEventListener("resize", function() {
+		W = window.innerWidth;
+		H = window.innerHeight;
+		canvas.width = W;
+		canvas.height = H;
+		colunms = Math.floor(W / fontSize);
+		drops = [];
+		for (var i = 0; i < colunms; i++) {
+			drops.push(Math.floor(canvas.height / fontSize) + 1);
+		}
+	});
+}())
